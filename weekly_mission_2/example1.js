ddc2d0994f437300a59f8c3771270231c8308501
AdrianGP5 <5adrian.pg@gmail.com>
2022-04-20 17:31:11 -0500

    objeto propio

--- weekly_mission_2/example1.js
+++ weekly_mission_2/example1.js
@@ -61,3 +61,26 @@ const repo = {
     
     console.log("DateCreated: " + PullRequest.getDateCreated())
     console.log("DateModified: " + PullRequest.getDateModified())
+
+
+    const Student = {
+        name: "Student",
+        age: 15,
+        id: 39632478,
+        lectures: ['Math', 'Science', 'Programming'],
+        getName: function(){
+            return this.name
+        },
+        getAge: function(){
+            return this.age
+        },
+        setName: function(_name){
+            this.name = _name
+        }
+    
+    }
+    console.log(Student)
+    console.log(`name: ${Student.getName()}`)
+    console.log("age: " + Student.getAge())
+    Student.setName(".::NewName::.")
+    console.log(`New name: ${Student.getName()}`)
