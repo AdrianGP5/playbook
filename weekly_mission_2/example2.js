0041e5bf06d849636af5a36b98cfec98b291c822
AdrianGP5 <5adrian.pg@gmail.com>
2022-04-22 14:01:30 -0500

    Adding filter

---
+++ weekly_mission_2/example2.js
@@ -0,0 +1,79 @@
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+
+   //E1
+   explorers.forEach(explorer => {
+       console.log(explorer.name)
+       console.log("...")
+   })
+
+   //E2
+   explorers.forEach(explorer => {console.log(explorer.stack)})
+
+   //E3 map crea lista 
+   console.log(explorers.map(explorer => explorer.stack)) 
+
+   //E4
+   console.log("Ejercicio 4")
+   const explorerWithJS = explorers.filter(explorerJS => explorerJS.stack.includes('js'))
+   console.log(explorerWithJS)
+   
+
