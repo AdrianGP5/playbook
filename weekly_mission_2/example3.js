5a8552ebd88d86bf5f4e00071f87160c26b53a03
AdrianGP5 <5adrian.pg@gmail.com>
2022-04-22 19:24:50 -0500

    simple object

---
+++ weekly_mission_2/example3.js
@@ -0,0 +1,12 @@
+//simple object
+const student = {
+    name: "Adrián",
+    age: 29,
+    idLaunchX: 1808,
+    email: "1808@launchx.com",
+    celPhone: '5512345678',
+    missions: ["onboarding", "front-end", "node-js"]
+}
+
+console.log("This is a simple object in JS:")
+console.log(student)
