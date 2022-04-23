185dc00849d4bb4b740a0255a4f405fd82eadcd3
AdrianGP5 <5adrian.pg@gmail.com>
2022-04-22 20:17:00 -0500

    constructor 2

--- weekly_mission_2/example3.js
+++ weekly_mission_2/example3.js
@@ -1,22 +1,22 @@
-//simple object
+//simple object ***********************************************************
 const student = {
     name: "Adrián",
     age: 29,
     idLaunchX: 1808,
     email: "1808@launchx.com",
-    celPhone: '5512345678',
+    cellPhone: '5512345678',
     missions: ["onboarding", "front-end", "node-js"]
 }
 console.log("This is a simple object in JS:")
 console.log(student)
 
-//Class without constructor
+//Class without constructor*****************************************************
 class Students{
     name
     age
     idLaunchX
     email
-    celPhone
+    cellPhone
     missions
 }
 console.log(Students)
@@ -26,7 +26,7 @@ student1.age = 21
 student1.missions = ["onboarding", "front-end"]
 console.log(student1)
 
-//Class with constructor without parameters
+//Class with constructor without parameters******************************************
 class StudentsL{
     constructor()
     {
@@ -36,14 +36,33 @@ class StudentsL{
     age
     idLaunchX
     email
-    celPhone
+    cellPhone
     missions
 }
 console.log(StudentsL)
 
 const studentL1 =  new StudentsL();
 studentL1.idLaunchX = 5090
-studentL1.celPhone = '5588445577'
+studentL1.cellPhone = '5588445577'
 console.log(studentL1)
+
+//Class with constructor and paramaters***********************************
+class StudentsLX{
+    constructor(name, age, idLaunchX, email, cellPhone, missions)
+    {
+        console.log("New object created!!!")
+        this.name = name
+        this.age = age
+        this.idLaunchX = idLaunchX
+        this.email = email
+        this.cellPhone =  cellPhone
+        this.missions = missions
+    }
+}
+console.log(StudentsLX)
+
+const StudentsLX1 = new StudentsLX('ADRIAN',29,1512,'1512@launchx.com',null,['onboarding','front-end','node-js'])
+console.log(StudentsLX1)
+
 
 
